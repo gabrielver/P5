045c6564c8fc83e5d9d3c9407b949d6300e70b5d
gabrielver <verronet.g@gmail.com>
2021-11-25 12:01:26 +0100

    product ok

--- front/js/product.js
+++ front/js/product.js
@@ -7,7 +7,47 @@ const id = urlParams.get('id');
 fetch("http://localhost:3000/api/products/" +id)
 .then(res => res.json())
 .then(product => {
-    console.log(product);
+    
+    //name of the product (top of the page)
+    const name = document.querySelector("title")
+    name.innerText = product.name
+    console.log(name);
+    //insert image
+    const itemImg = document.querySelector(".item__img");
+    console.log(itemImg);
+    const img = document.createElement("img");
+    itemImg.appendChild(img);
+    img.src = "../images/logo.png";
+    img.alt = "Photographie d'un canapé";
+
+    //nom du produit
+    const title = document.getElementById("title");
+    title.innerText= product.name;
+    //price
+    const price = document.getElementById("price");
+    price.innerText= product.price;
+    //description
+    const desc = document.getElementById("description");
+    desc.innerText = product.description;
+    //color select
+    const array1 = product.colors;
+    array1.forEach(element => {
+        const option = document.getElementById("colors");
+        const col = document.createElement("option");
+        option.appendChild(col)
+        col.innerHTML = element;
+        col.value = element;
+    
+    });
+
+   
+    
+    
+   
+
+
+
+    
 });
 
-      
+    
