cc39e48552ac78f98e45fb684b39e5394fc3fa59
gabrielver <verronet.g@gmail.com>
2021-12-20 12:37:09 +0100

    form ok and comments on cart.html

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,16 +3,14 @@ if(window.location.href.includes("confirmation.html")) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const id = urlParams.get('id');
+  //we get back the orderId from the url and display it where it is supposed to appear on our page 
   document.getElementById("orderId").innerHTML = id;
-
-
 }
 
 //get the local storage back
 Object.keys(localStorage).forEach(function(key) {
     let item_product = JSON.parse(localStorage.getItem(key));
-
-    //1. recupère les infos du produit via l'api à l'aide de item_product.idItem
+    //get the product info using the API and the item_product.idItem
     fetch("http://localhost:3000/api/products/" +item_product.idItem)
         .then(res => res.json())
         .then(product => {
@@ -88,8 +86,10 @@ Object.keys(localStorage).forEach(function(key) {
             input.min = "1";
             input.max = "100";
             input.value = item_product.quantity;
+            //we use the dataset (id and color) to target exactly what we need to modify in the localstorage
             input.dataset.key = item_product.idItem + '_' + item_product.color;
             quantity.appendChild(input);
+            //when we use the input the change the quantity, it will be updated in the local storage and same for the total price / total quantity
             input.onchange = changeQuantity
 
 
@@ -102,7 +102,9 @@ Object.keys(localStorage).forEach(function(key) {
             const deleteItem = document.createElement("p");
             deleteItem.className = "deleteItem";
             deleteItem.innerHTML = "Supprimer";
+            ///we use it to delete the HTML and the corresponding "key" in the local storage
             deleteItem.onclick = deleteRow
+            //we use the dataset (id and color) to target exactly what we need to delete in the localstorage
             deleteItem.dataset.key = item_product.idItem + '_' + item_product.color;
             del.appendChild(deleteItem);
             
@@ -111,16 +113,16 @@ Object.keys(localStorage).forEach(function(key) {
 });
 
 updateDisplay();
-
+//this will allow us to modify the quantity of an item on the page and in the local storage  
 function changeQuantity() {
     const key = this.dataset.key;
     const item = JSON.parse(localStorage.getItem(key));
     item.quantity = parseInt(this.value);
     localStorage.setItem(key, JSON.stringify(item));
-
+    //then we update the total price and total quantity
     updateDisplay();
 }
-
+//this function will uptate the total price and total quantity after we change the quantity or if we delete an item
 function updateDisplay() {
     let total_quantity = 0;
     let total_price    = 0;
@@ -134,80 +136,87 @@ function updateDisplay() {
     document.getElementById("totalQuantity").innerHTML  = total_quantity;
     document.getElementById("totalPrice").innerHTML     = total_price;
 }
-
+//we use it to delete the HTML and the corresponding "key" in the local storage
 function deleteRow() {
     const key = this.dataset.key;
     localStorage.removeItem(key);
-
     document.querySelector('article[data-key="'+ key +'"').remove();
+    //this function will uptate the total price and total quantity after we deleted an item
     updateDisplay();
 }
 
+//FORM
+//create a fonction to disable the submit button
+function disableSubmit(disabled) {
+    if (disabled) {
+      order = document.getElementById("order")
+        order.setAttribute("disabled", true);
+    } else {
+        order = document.getElementById("order")
+        order.removeAttribute("disabled");}
+  }
+  //check the input, if they doesn't contain the right caracter, submit is disable. if not, submit works 
+  firstName = document.getElementById("firstName")
+  firstName.addEventListener("input", function(e) {
+    if (/^[a-z\-_\s]+$/i.test(e.target.value)) {
+      e.target.nextElementSibling.innerText = "";
+      disableSubmit(false);
+    } else {
+        e.target.nextElementSibling.innerText = "Ce champs ne doit pas contenir de caractères spéciaux, merci";
+      disableSubmit(true);}
+  });
+//check the input, if they doesn't contain the right caracter, submit is disable. if not, submit works
+  lastName = document.getElementById("lastName")
+  lastName.addEventListener("input", function(e) {
+    if (/^[a-z\-_\s]+$/i.test(e.target.value)) {
+      e.target.nextElementSibling.innerText = "";
+      disableSubmit(false);
+    } else {
+        e.target.nextElementSibling.innerText = "Ce champs ne doit pas contenir de caractères spéciaux, merci";
+      disableSubmit(true);}
+  });
+//check the input, if they doesn't contain the right caracter, submit is disable. if not, submit works
+  address = document.getElementById("address")
+  address.addEventListener("input", function(e) {
+    if (/^[a-z\d\-_\s]+$/i.test(e.target.value)) {
+      e.target.nextElementSibling.innerText = "";
+      disableSubmit(false);
+    } else {
+        e.target.nextElementSibling.innerText = "Ce champs ne doit pas contenir de caractères spéciaux, merci";
+      disableSubmit(true);}
+  });
+//check the input, if they doesn't contain the right caracter, submit is disable. if not, submit works
+  city = document.getElementById("city")
+  city.addEventListener("input", function(e) {
+    if (/^[a-z\-_\s]+$/i.test(e.target.value)) {
+      e.target.nextElementSibling.innerText = "";
+      disableSubmit(false);
+    } else {
+        e.target.nextElementSibling.innerText = "Ce champs ne doit pas contenir de caractères spéciaux, merci";
+      disableSubmit(true);}
+  });
 
-//CHECK THE FORM
-function checkForm(){
-//get all the input by text or required
+  
+//make sure to disable the submit button if the inputs are empty
 const allInput = document.querySelectorAll("input[type=text]");
 allInput.forEach(function (e){
- //e.required = true;
-// if(e.value === ""){
-//   e.nextElementSibling.innerHTML = "Ce champs est obligatoire et ne doit pas contenir de caractères spéciaux, merci";}
-  //for each input, we check if the value entered by the buyer is correct
-  e.addEventListener("input", function() {
-    if (/^[a-z\-_\s]+$/i.test(e.value)) {
-    e.nextElementSibling.innerHTML = "";
-    //AUTORISER LE BOUTON A ETRE CLICKABLE OU LE FORMULAIRE SE VALIDE 
-    //for the input "address", we allow numbers 
-    }else if(e === address){
-       if (/^[a-z\d\-_\s]+$/i.test(e.value)){
-          e.nextElementSibling.innerHTML = "";
-          
-        }else{
-          e.nextElementSibling.innerHTML = "Ce champs est obligatoire et ne doit pas contenir de caractères spéciaux, merci";}
-          //if the value of the input doesn't match the regex or is null, a message appear  
-    }else {
-      //LE BOUTTON NE DOIT PAS ETRE CLICKABLE OU EL FORMULAIRE NE SE VALIDE PAS
-    e.nextElementSibling.innerHTML = "Ce champs est obligatoire et ne doit pas contenir de caractères spéciaux, merci"
-    
-  }
-  });
+if(e.value === ""){
+    disableSubmit(true);
+}else{
+    disableSubmit(false);}
 });
-}
 
-
-
-
- checkForm();
-
-//on click, check the  form input
- formOrder = document.querySelector("#order");
-// console.log(formOrder)
-// if(formOrder === null) {
-formOrder.addEventListener("click", checkForm);
-// }
-
-// form = document.querySelector(".cart__order__form");
-// if(form === null) {
-//     form.addEventListener("submit", send);
-// }
+//if there is no products in the local storage, submit is disable
+if (localStorage.length == 0) {
+    disableSubmit(true);}
+ 
+//If the submit button is enable (the form is right), we use the sent function to send our data to the API
 form = document.querySelector(".cart__order__form");
 form.addEventListener("submit", send);
-/*
-let key = "77711f0e466b4ddf953f677d30b0efc9";
-console.log('article[data-key="'+ key +'"');
-let quantity = document.querySelector('article[data-key="'+ key +'"');
-console.log(quantity);*/
-
-//FORM
-
-// //get acces to the products in the local storage
-
 
 //Create a function SEND that will contain the POST
 function send(e) {
     e.preventDefault();
-
-    checkForm();
 
     //Create an object with the info from the form
     let firstname = document.getElementById("firstName").value;
@@ -215,7 +224,7 @@ function send(e) {
     let addresse = document.getElementById("address").value;
     let city = document.getElementById("city").value;
     let email = document.getElementById("email").value;
-
+    // the object "contact" we are going to send
     let formData =
         {
             firstName: firstname,
@@ -224,18 +233,18 @@ function send(e) {
             city: city,
             email: email
         };
-
+    //the object "product" that we are going to send
     let products = getProducts();
     let product_ids = [];
     products.forEach(function (product) {
         product_ids.push(product.idItem);
     });
-
+    //the body contain the contact and products objects that we need to send to the API
     let body = {
         contact: formData,
         products: product_ids
     };
-
+    //we use the POST method o send the info
     fetch("http://localhost:3000/api/products/order", {
         method: "POST",
         headers: {
@@ -251,17 +260,20 @@ function send(e) {
         }
     })
     .then(function (data) {
+        //we get the data back (the orderId that we need should be in it)
         console.log(data.orderId);
+        //we use this function to go to the confirmation page while using tge orderId 
         goToConfirmation(data.orderId);
-        // localStorage.clear();
+        //We clear the local storage of our odrer
+        localStorage.clear();
     })
 }
 
-//function to go to the confiramtion page
+//function to go to the confiramtion page while using the orderId
 function goToConfirmation(orderId) {
     window.location.href = "confirmation.html?id=" + orderId;
 }
-
+//function we use the get the products and to be able to use them in the function send()
 function getProducts() {
     let products = [];
     Object.keys(localStorage).forEach(function (key) {
@@ -271,12 +283,54 @@ function getProducts() {
     return products;
 }
 
+
+
+
+
+//CHECK THE FORM
+// function checkForm(){
+
+// //get all the input by text or required
+// const allInput = document.querySelectorAll("input[type=text]");
+// allInput.forEach(function (e){
+//  //e.required = true;
+//   //for each input, we check if the value entered by the buyer is correct
+//   e.addEventListener("input", function() {
+//     if (/^[a-z\-_\s]+$/i.test(e.value)) {
+//     e.nextElementSibling.innerHTML = "";
+   
+//     return true;
+//     //AUTORISER LE BOUTON A ETRE CLICKABLE OU LE FORMULAIRE SE VALIDE 
+//     //for the input "address", we allow numbers 
+//     }else if(e === address){
+//        if (/^[a-z\d\-_\s]+$/i.test(e.value)){
+//           e.nextElementSibling.innerHTML = ""; 
+         
+//           return true;
+//         }else{   
+//           e.nextElementSibling.innerHTML = "Ce champs ne doit pas contenir de caractères spéciaux, merci";
+//           alert("essai encore");
+//           return false;}
+//           //if the value of the input doesn't match the regex or is null, a message appear  
+//     }else {  
+//       //LE BOUTTON NE DOIT PAS ETRE CLICKABLE OU EL FORMULAIRE NE SE VALIDE PAS
+//     e.nextElementSibling.innerHTML = "Ce champs ne doit pas contenir de caractères spéciaux, merci";
+//     alert("essai encore");
+//     return false;
+//   }
+//   });
+
+// });
+// }
+
+///////////////////////////////////
+
 //function to check if the form is well completed
-// function checkForm() {
+// function checkForm(e) {
 //     let firstname = document.getElementById("firstName");
-//     if (firstname.value === "") {
-//         document.getElementById("firstNameErrorMsg").innerHTML = "Ce champs est obligatoire, merci";
-//     }
+//     if (firstname.value == /^[a-z\-_\s]+$/i.test) {
+//         document.getElementById("firstNameErrorMsg").innerHTML = "";
+//     }else{document.getElementById("firstNameErrorMsg").innerHTML = "Ce champs est obligatoire, merci";}
     
 
 //     let lastName = document.getElementById("lastName");
@@ -298,67 +352,17 @@ function getProducts() {
 //     if (email.value === "") {
 //         document.getElementById("emailErrorMsg").innerHTML = "Ce champs est obligatoire, merci";
 //     }
+
 // }
 
 
+  
+//   //get all the input by text or required
+// const allInput = document.querySelectorAll("input[type=text]");
+// allInput.forEach(function (e){
 
-
-
-//////////////////////////////////////////////////////////
-// allInput.addEventListener("input", function(e) {
-//     if (/^\p{L}+$/u.test(e.target.value)) {
-//       document.getElementById("firstNameErrorMsg").innerHTML = "";
-//     console.log(target)
-//   } else {
-//     document.getElementById("firstNameErrorMsg").innerHTML = "Ce champs est obligatoire, merci";
-    
-//   }
-// });
-
-////////////////////////////////////////////////////////////////////
-// firstname = document.getElementById("firstName");
-// firstname.addEventListener("input", function(e) {
-//     if (/^\p{L}+$/u.test(e.target.value)) {
-//       document.getElementById("firstNameErrorMsg").innerHTML = "";
-    
-//   } else {
-//     document.getElementById("firstNameErrorMsg").innerHTML = "Ce champs est obligatoire, merci";
-    
-//   }
-// });
-
-
-
-// let firstname = document.getElementById("firstName");
-// firstname.addEventListener("keyup", getRidOfMessage);
-
-// function getRidOfMessage (){
-//   let error = document.querySelector("#firstNameErrorMsg");
-//  error.innerHTML.remove;
-// console.log(error);
-// };
-
-
-// // window.addEventListener("keyup", log);
-
-// function log(event){
-//   console.log( event.type );
-// }
-
-
-
-//getProducts();
-// function getOrderId(){
-//   fetch("http://localhost:3000/api/products/order")
-//     .then(function(res) {
-//       if (res.ok) {
-//         return res.json();
-//       }
-//     })
-//     .then(function(value) {
-//       console.log(value);
-//     })
-//     .catch(function(err) {
-//       // Une erreur est survenue
+//   e.addEventListener("input", function() {
+//     if (/^[a-z\-_\s]+$/i.test(e.value)) {
+//     e.nextElementSibling.innerHTML = "";
 //     });
-// }
+// });
